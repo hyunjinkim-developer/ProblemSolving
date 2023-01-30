52a596482a153e98ca2a845de24f06772de5d085
Jin Kim <hellothisisjin@gmail.com>
2023-01-30 17:59:13 +0900

    chap5-6

---
+++ chap5-6_1.py
@@ -0,0 +1,9 @@
+INF = 999999999 # Set infinity value for edges not connected
+
+# Adjacncy Matrix using 2 dimensional array (in this case list)
+graph = [[0, 7, 5], 
+		 [7, 0, INF], 
+		 [5, INF]
+]
+
+print(graph)
