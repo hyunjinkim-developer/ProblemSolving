96497d088f9a244b5e3f581575d20ef76de4826a
Jin Kim <hellothisisjin@gmail.com>
2023-01-31 11:15:04 +0900

    chap5-7

---
+++ chap5-7_1.py
@@ -0,0 +1,15 @@
+# Linked list with 3-rows 2 dimensional array
+graph = [[] for _ in range(3)]
+
+# Save nodes
+# Node 0
+graph[0].append((1, 7))
+graph[0].append((2, 5))
+
+# Node 1
+graph[1].append((0, 7))
+
+# Node 2
+graph[2].append((0, 5))
+
+print(graph)
