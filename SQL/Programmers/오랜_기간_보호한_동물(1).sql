90136cb59aecd7d7a8e75eb9706c7c6cf8c75494
Jin <hellothisisjin@gmail.com>
2023-09-17 18:19:49 +0900

    feat: 오랜_기간_보호한_동물(1)

---
+++ SQL/Programmers/오랜_기간_보호한_동물(1).sql
@@ -0,0 +1,18 @@
+-- 아직 입양을 못 간 동물 중,
+-- 가장 오래 보호소에 있었던 동물 3마리의 이름과 보호 시작일을 조회
+-- 결과는 보호 시작일 순으로 조회해야 합니다.
+
+SELECT ANIMAL_INS.NAME, ANIMAL_INS.DATETIME
+FROM ANIMAL_INS
+LEFT JOIN ANIMAL_OUTS
+ON ANIMAL_INS.ANIMAL_ID = ANIMAL_OUTS.ANIMAL_ID
+WHERE ANIMAL_OUTS.ANIMAL_ID IS NULL
+ORDER BY ANIMAL_INS.DATETIME ASC
+LIMIT 3;
+
+-- Difference Set in PostgreSQL
+# SELECT select_list
+# FROM A
+# EXCEPT
+# SELECT select_list
+# FROM B;
