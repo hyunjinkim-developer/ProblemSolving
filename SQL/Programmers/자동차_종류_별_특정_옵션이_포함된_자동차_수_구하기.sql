57a8201e622b1151972645ec23afe7c673487b89
Jin <hellothisisjin@gmail.com>
2023-09-23 18:32:11 +0900

    feat: Programmers SQL 자동차 종류 별 특정 옵션이 포함된 자동차 수 구하기

---
+++ SQL/Programmers/자동차_종류_별_특정_옵션이_포함된_자동차_수_구하기.sql
@@ -0,0 +1,11 @@
+-- CAR_RENTAL_COMPANY_CAR 테이블에서 '통풍시트', '열선시트', '가죽시트' 중 하나 이상의 옵션이 포함된 자동차
+-- 자동차 종류 별로 몇 대
+-- 자동차 수에 대한 컬럼명은 CARS로 지정하고, 결과는 자동차 종류를 기준으로 오름차순 정렬
+
+SELECT CAR_TYPE, COUNT(CAR_ID) AS "CARS"
+FROM CAR_RENTAL_COMPANY_CAR
+-- There's only 3 "시트" which is 통풍시트', '열선시트', '가죽시트'
+-- the OPTIONS column consist of string, so "%시트%" is appropriate
+WHERE OPTIONS LIKE "%시트%"
+GROUP BY CAR_TYPE
+ORDER BY CAR_TYPE ASC
