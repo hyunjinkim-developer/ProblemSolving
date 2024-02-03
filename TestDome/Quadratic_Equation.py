e1378300587724c524fe90f488f998211cd85dbb
Jin <hellothisisjin@gmail.com>
2024-02-03 18:15:36 +0900

    feat: TestDome

---
+++ TestDome/Quadratic_Equation.py
@@ -0,0 +1,25 @@
+"""
+Problem:
+https://www.testdome.com/questions/python/quadratic-equation/94864
+"""
+
+# Solution 1:
+
+def find_roots(a, b, c):
+  x1 = (b * (-1) + ((b ** 2) - (4 * a * c)) ** 0.5) / (2 * a)
+  x2 = (b * (-1) - ((b ** 2) - (4 * a * c)) ** 0.5) / (2 * a)
+  return (x1, x2)
+
+print(find_roots(2, 10, 8));
+
+"""
+# Solution 2:
+
+import numpy as np
+
+def find_roots(a, b, c):
+  input = list([a, b, c])
+  return tuple(np.roots(input))
+
+print(find_roots(2, 10, 8));
+"""
