1d4e67127e6634f0c0150e0832f938681ff20865
Jin <hellothisisjin@gmail.com>
2024-02-05 17:45:04 +0900

    feat: TestDome

---
+++ TestDome/!Pipeline.py
@@ -0,0 +1,17 @@
+"""
+Problem:
+https://www.testdome.com/questions/python/pipeline/94856
+"""
+
+
+def pipeline(*funcs):
+    def helper(arg):
+        for func in funcs:
+            arg = func(arg)
+        return arg
+
+    return helper
+
+
+fun = pipeline(lambda x: x * 3, lambda x: x + 1, lambda x: x / 2)
+print(fun(3))  # should print 5.0
