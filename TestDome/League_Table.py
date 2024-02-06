145aef27469e90b701a25f42640926beb26d3bc4
Jin <hellothisisjin@gmail.com>
2024-02-06 12:36:43 +0900

    feat: TestDoem sorting additional answers

--- TestDome/League_Table.py
+++ TestDome/League_Table.py
@@ -3,6 +3,9 @@ Problem:
 https://www.testdome.com/questions/python/league-table/94851
 """
 
+"""
+# Solution 1:
+"""
 from collections import Counter
 from collections import OrderedDict
 
@@ -39,3 +42,33 @@ if __name__ == "__main__":
     table.record_result('Arnold', 5)
     table.record_result('Chris', 5)
     print(table.player_rank(1))
+
+
+
+"""
+# Solution 2:
+
+from collections import Counter
+from collections import OrderedDict
+
+class LeagueTable:
+    def __init__(self, players):
+        self.standings = OrderedDict([(player, Counter()) for player in players])
+       
+    def record_result(self, player, score):
+        self.standings[player]['games_played'] += 1
+        self.standings[player]['score'] += score
+     
+    def player_rank(self, rank):
+      ranks = [(-counter['score'], counter['games_played'], idx, name)
+                for idx, (name, counter) in enumerate(self.standings.items())]   
+      return sorted(ranks)[rank - 1][3]
+
+if __name__ == "__main__":
+    table = LeagueTable(['Mike', 'Chris', 'Arnold'])
+    table.record_result('Mike', 2)
+    table.record_result('Mike', 3)
+    table.record_result('Arnold', 5)
+    table.record_result('Chris', 5)
+    print(table.player_rank(1))
+"""
