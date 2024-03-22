9e39d847bd23e1e8e268b345ebc56100d214dfda
Jin <hellothisisjin@gmail.com>
2024-03-22 19:54:57 +0900

    feat: add sample solution

--- PythonCodingTest/4_Simulation/q13_치킨배달.py
+++ PythonCodingTest/4_Simulation/q13_치킨배달.py
@@ -1,7 +1,11 @@
 """
 Problem: https://www.acmicpc.net/problem/15686
+    # N(2 ≤ N ≤ 50)과 M(1 ≤ M ≤ 13)
+    # 집의 개수는 2N개를 넘지 않으며
+    # 치킨집의 개수는 M보다 크거나 같고, 13보다 작거나 같다.
 """
 
+"""
 # Solution 1:
 from itertools import combinations
 
@@ -39,12 +43,6 @@ def calculate_chicken_distance(group):
 def select_store():
     global stores, M
     min_city_chicken_distance = int(1e9)
-    # N(2 ≤ N ≤ 50)과 M(1 ≤ M ≤ 13)
-    # 집의 개수는 2N개를 넘지 않으며
-    # 치킨집의 개수는 M보다 크거나 같고, 13보다 작거나 같다.
-    # chicken_distance 최대 2 * N 이라고 하면
-    # 각 치킨집에서 집까지 chicken_distance의 합: (2 * N) * (2 * N) := 4 * N^2
-    # M개의 치킨집을 선택하면 total_chicken_distance: M * (4 * N^2) ~ O(N^3)
 
     store_groups = list(combinations(stores, M))
     for group in store_groups:
@@ -54,6 +52,54 @@ def select_store():
 def main():
     get_input()
     print(select_store())
+"""
+
+# Solution 2: Sample Solution
+from itertools import combinations
+
+N, M = 0, 0
+houses, stores = [], []
+
+def get_input():
+    global N, M, houses, stores
+
+    # Initialization
+    N, M = 0, 0
+    houses, stores = [], []
+
+    N, M = map(int, input().split())
+    for row in range(N):
+        data = list(map(int, input().split()))
+        for col in range(N):
+            if data[col] == 1:
+                houses.append((row, col))
+            elif data[col] == 2:
+                stores.append((row, col))
+
+def select_store():
+    global N, M, houses, stcores
+    answer = int(1e9)
+
+    def calculate_city_chicken_distance(group):
+        city_chicken_distance = 0
+
+        for h_r, h_c in houses:
+            chicken_distance = int(1e9)
+            for s_r, s_c in group:
+                chicken_distance = min(chicken_distance, abs(h_r - s_r) + abs(h_c - s_c))
+            city_chicken_distance += chicken_distance
+        return city_chicken_distance
+
+    store_groups = list(combinations(stores, M))
+    for group in store_groups:
+        # print(group)
+        answer = min(answer, calculate_city_chicken_distance(group))
+    return answer
+
+def main():
+    get_input()
+    print(select_store())
+
 
 
 import sys
