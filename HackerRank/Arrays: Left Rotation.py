de370f0b034bc79baee1edf67f11aa760e99c2a3
Jin <hellothisisjin@gmail.com>
2024-05-04 19:28:15 +0900

    feat: LeetCode
    Former-commit-id: f7b46729cf8cd31d82e36ca506b551b95c735333

---
+++ HackerRank/Arrays: Left Rotation.py
@@ -0,0 +1,44 @@
+# https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'rotLeft' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER d
+
+
+from collections import deque
+def rotLeft(a, d):
+    a = deque(a)
+    for _ in range(d):
+        num = a.popleft()
+        a.append(num)
+    return list(a)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = rotLeft(a, d)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
