aa565a95a02cb06c4f264d2c39e9499e47fc5265
Jin <hellothisisjin@gmail.com>
2024-06-19 11:21:37 +0900

    chore: add comment, key point for the question

--- CodeTree/정육면체한번더굴리기/정육면체한번더굴리기.py
+++ CodeTree/정육면체한번더굴리기/정육면체한번더굴리기.py
@@ -1,7 +1,12 @@
-# SWEA 모의 SW역량 테스트 baseline
-
-# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-# 아래 표준 입출력 예제 필요시 참고하세요.
+"""
+# 문제의 Point
+* Roll dice in given direction
+    Data structure to save dice's bottom, L, R, U, D side for each rolling
+    - dice_bottom := int
+    - dice := dictionary key: "L", "R", "U", "D", value: number of each side based on dice_bottom
+* Count adjacent locations that has the same board score as dice_bottom
+    Use BFS algorithm implementing with deque()
+"""
 
 # 표준 입력 예제
 '''
